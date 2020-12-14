6fb83315667f08a9b837f46b6ddf69999ee95c02
Akita Emmanuel <e.akita91@gmail.com>
2020-12-14 18:18:15 +0100

    Add post order

--- src/controller/order.ts
+++ src/controller/order.ts
@@ -13,46 +13,54 @@ export default class OrderController {
     @summary("Find all orders")
     public static async getOrders(ctx: BaseContext): Promise<void> {
 
-        // get databasee reefrence for order
+        // get database reference for order
         const db = admin.database();
         const orders = db.ref(`orders`);
 
         // load all orders
         orders.on("value", function (snapshot) {
-            console.log(snapshot.val());
+            // return OK status code and loaded orders array
             ctx.status = 200;
             ctx.body = snapshot.val();
-        }, function (errorObject) {
-            console.log("The read failed: " + errorObject);
+        }, function (error) {
+                ctx.status = 400;
+                ctx.body = error;
         });
 
-        // return OK status code and loaded users array
+       
 
     }
 
     @request("get", "/orders/{uid}")
-    @summary("Find order by id")
+    @summary("Find order by uid")
     @path({
-        id: { type: "number", required: true, description: "id of order" }
+        id: { type: "string", required: true, description: "uid of order" }
     })
     public static async getOrder(ctx: BaseContext): Promise<void> {
 
-        // get databasee reefrence for order
+        // get database reference for order
         const db = admin.database();
-        const orders = db.ref(`orders`);
+        const orders = db.ref(`orders` + +ctx.params.uid );
 
         // load order by id
-        // const user: User | undefined = await userRepository.findOne(+ctx.params.id || 0);
-
-        if (orders) {
-            // return OK status code and loaded order object
-            ctx.status = 200;
-            ctx.body = orders;
-        } else {
-            // return a BAD REQUEST status code and error message
+        orders.on("value", function (snapshot) {
+            // return OK status code and loaded users array
+         const order = snapshot.val();
+            if (order) {
+                // return OK status code and loaded order object
+                ctx.status = 200;
+                ctx.body = order;
+            } else {
+                // return a BAD REQUEST status code and error message
+                ctx.status = 400;
+                ctx.body = "The order you are trying to retrieve doesn't exist in the db";
+            }
+        }, function (error) {
             ctx.status = 400;
-            ctx.body = "The order you are trying to retrieve doesn't exist in the db";
-        }
+            ctx.body = error;
+        });
+
+
 
     }
 
@@ -61,7 +69,7 @@ export default class OrderController {
     @body(orderSchema)
     public static async createOrder(ctx: BaseContext): Promise<void> {
 
-        // get databasee reefrence for order
+        // get database reference for order
         const db = admin.database();
         const orders = db.ref(`orders`);
 
@@ -69,21 +77,21 @@ export default class OrderController {
 
        const orderToBeSaved = {
             address: {
-                city: ctx.request.body.address.city,
-                country: ctx.request.body.address.country,
-                street: ctx.request.body.address.street,
-                zip: ctx.request.body.address.zip
+                city: ctx.request.body.city,
+                country: ctx.request.body.country,
+                street: ctx.request.body.street,
+                zip: ctx.request.body.zip
             },
             bookingDate: ctx.request.body.bookingDate,
             customer: {
-                name: ctx.request.body.customer.name,
-                phone: ctx.request.body.customer.phone,
-                email: ctx.request.body.customer.email
+                name: ctx.request.body.name,
+                phone: ctx.request.body.phone,
+                email: ctx.request.body.email
             },
             title: ctx.request.body.title,
         }
 
-        // validate user entity
+        // validate order
         const errors: ValidationError[] = await validate(orderToBeSaved); // errors is an array of validation errors
 
         if (errors.length > 0) {
@@ -91,9 +99,9 @@ export default class OrderController {
             ctx.status = 400;
             ctx.body = errors;
         } else {
-            // save the user contained in the POST body
-            const order = orders.set(orderToBeSaved);
-            // return CREATED status code and updated user
+            // save the order contained in the POST body
+            const order = orders.push(orderToBeSaved);
+            // return CREATED status code and updated order
             ctx.status = 201;
             ctx.body = order;
         }
@@ -102,7 +110,7 @@ export default class OrderController {
     @request("put", "/orders/{uid}")
     @summary("Update an order")
     @path({
-        id: { type: "number", required: true, description: "id of order" }
+        id: { type: "string", required: true, description: "id of order" }
     })
     @body(updateOrderSchema)
     public static async updateOrder(ctx: BaseContext): Promise<void> {
@@ -139,59 +147,12 @@ export default class OrderController {
         // } 
         else {
             // save the user contained in the PUT body
-            const order = orders.set(orderToBeUpdated);
+            const order = orders.update(orderToBeUpdated);
             // return CREATED status code and updated user
             ctx.status = 201;
             ctx.body = order;
         }
 
     }
-
-    // @request("delete", "/users/{id}")
-    // @summary("Delete user by id")
-    // @path({
-    //     id: { type: "number", required: true, description: "id of user" }
-    // })
-    // public static async deleteUser(ctx: BaseContext): Promise<void> {
-
-    //     // get a user repository to perform operations with user
-    //     const userRepository = getManager().getRepository(User);
-
-    //     // find the user by specified id
-    //     const userToRemove: User | undefined = await userRepository.findOne(+ctx.params.id || 0);
-    //     if (!userToRemove) {
-    //         // return a BAD REQUEST status code and error message
-    //         ctx.status = 400;
-    //         ctx.body = "The user you are trying to delete doesn't exist in the db";
-    //     } else if (ctx.state.user.email !== userToRemove.email) {
-    //         // check user's token id and user id are the same
-    //         // if not, return a FORBIDDEN status code and error message
-    //         ctx.status = 403;
-    //         ctx.body = "A user can only be deleted by himself";
-    //     } else {
-    //         // the user is there so can be removed
-    //         await userRepository.remove(userToRemove);
-    //         // return a NO CONTENT status code
-    //         ctx.status = 204;
-    //     }
-
-    // }
-
-    // @request("delete", "/testusers")
-    // @summary("Delete users generated by integration and load tests")
-    // public static async deleteTestUsers(ctx: BaseContext): Promise<void> {
-
-    //     // get a user repository to perform operations with user
-    //     const userRepository = getManager().getRepository(User);
-
-    //     // find test users
-    //     const usersToRemove: User[] = await userRepository.find({ where: { email: Like("%@citest.com")} });
-
-    //     // the user is there so can be removed
-    //     await userRepository.remove(usersToRemove);
-
-    //     // return a NO CONTENT status code
-    //     ctx.status = 204;
-    // }
 
 }
