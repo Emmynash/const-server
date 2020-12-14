1bbc5ebba90873ed6a53f1eaea2cb161a9873ede
Akita Emmanuel <e.akita91@gmail.com>
2020-12-14 21:54:29 +0100

    Delete redundant codes

--- src/unprotectedRoutes.ts
+++ src/unprotectedRoutes.ts
@@ -1,16 +1,18 @@
 import Router from "@koa/router";
 import { general } from "./controller";
-import {order} from './controller'
+import { order } from './controller'
 
 const unprotectedRouter = new Router();
-
-// Hello World route
-unprotectedRouter.get("/", general.helloWorld);
 
 // order route
 unprotectedRouter.get("/orders", order.getOrders);
 unprotectedRouter.get("/orders/:uid", order.getOrder);
 unprotectedRouter.post("/orders", order.createOrder);
 unprotectedRouter.put("/orders/:uid", order.updateOrder);
+
+// Hello World route
+unprotectedRouter.get("/", general.helloWorld);
+
+
 
 export { unprotectedRouter };
