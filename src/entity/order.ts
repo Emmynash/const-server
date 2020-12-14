b74bf1ba0a59dfb92f85282cfc88990fee19a677
Akita Emmanuel <e.akita91@gmail.com>
2020-12-14 18:19:20 +0100

    remove array from orderSchema

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -21,22 +21,18 @@ export class User {
 }
 
 export const orderSchema = {
-    address: {
         city: { type: "string", required: true, example: "Berlin" },
         country: { type: "string", required: true, example: "Germany" },
         street: { type: "string", required: true, example: "Wriezener str. 12" },
-        zip: { type: "string", required: true, example: "13055" }
-    },
-    bookingDate: { type: "number", required: true, example: 1 },
-    customer: {
+        zip: { type: "string", required: true, example: "13055" },
+        bookingDate: { type: "number", required: true, example: 1554284950000 },
         name: { type: "string", required: true, example: "Emmanuel Akita" },
         email: { type: "string", required: true, example: "e.akita@email.com" },
-        phone: { type: "string", required: true, example: "0123456789" }
-    },
-    title: { type: "string", required: true, example: "Test order 1" },
+        phone: { type: "string", required: true, example: "0123456789" },
+        title: { type: "string", required: true, example: "Test order 1" },
 };
 
 export const updateOrderSchema = {
-    bookingDate: { type: "number", required: true, example: 1 },
+    bookingDate: { type: "number", required: true, example: 1554284950000 },
     title: { type: "string", required: true, example: "Test order 1" },
 }
